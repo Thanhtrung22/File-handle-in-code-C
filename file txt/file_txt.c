d19e6421878dc5248ea4eab8413b40391f619c27
ThanhTrung22 <anhtrungpk11@gmail.com>
2024-05-24 17:15:14 +0700

    add file txt

---
+++ file txt/file_txt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void read_file(FILE *f)
+{
+    int temp = 0;
+    while(fscanf(f, "%d", &temp) > 0)
+    {
+        
+        printf("%d ", temp);
+    }
+    
+}
+
+void write_file(FILE *f)
+{
+    int temp = 0;
+    for(int i = 0; i < 100; i++)
+    {
+        temp =  2 * i + 1;
+        int check = fprintf(f, "%d ", temp);
+        if(check < 0)
+        {
+            printf("\nError writing file");
+            break;
+        }
+
+    }
+}
+int main()
+{
+    FILE *file = fopen("file.txt", "w+");
+    if(file == NULL)
+    {
+        printf("\nOpen file failed");
+        return -1;
+
+    }
+    write_file(file);
+    rewind(file);
+    read_file(file);
+    fclose(file);
+    
+   
+    return 0;
+}
