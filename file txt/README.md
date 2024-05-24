d25bbb10acbe8192620e5a0883a922eed8271c4c
ThanhTrung22 <anhtrungpk11@gmail.com>
2024-05-24 23:33:09 +0700

    complete

---
+++ file txt/README.md
@@ -0,0 +1,8 @@
+#  Program import files and exports files for students
+## Import files 
+### -Enter name file which needs to be input
+### -Handle that file and inform to screen
+  - Note: Process buffer array before next doing
+## Export files
+## Display list to screen
+## Exit program
--- file txt/import_export_file.c
+++ file txt/import_export_file.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include  <string.h>
 #include <stdbool.h>
+#include <conio.h>
+#include <stdlib.h>
+
 #define MAX 1000
 
-char input_file[1024];
-char output_file[1024];
+
 
 typedef struct
 {
@@ -17,37 +19,96 @@ void import_file(FILE *file, student *list, unsigned *quantity);
 void export_file(FILE *file, student *list, unsigned quantity);
 void display(student *list, unsigned quantity);
 bool check_empty_file(FILE *file);
+void menu();
 int main()
 {
     student list[MAX];
-    FILE *file = NULL;
-    printf("Nhap ten file input: ");
-    fgets(input_file, sizeof(input_file), stdin);
-    if(input_file[strlen(input_file) - 1] == '\n')
+    unsigned quantity = 0;
+    char input_file[1024];
+    char output_file[1024];
+    FILE *file_input = NULL, *file_output = NULL;
+    char sel = '\0';
+    while(1)
     {
-        input_file[strlen(input_file) - 1] = '\0';
+        system("cls");
+        menu();
+        printf("\nNhap lua chon cua ban: ");
+        sel = (char) getche();
+        switch(sel)
+        {
+            case '1':
+            {
+                printf("\nNhap ten file input: ");
+                //while(getchar() != '\n');
+                fgets(input_file, sizeof(input_file), stdin);
+                if(input_file[strlen(input_file) - 1] == '\n')
+                {
+                     input_file[strlen(input_file) - 1] = '\0';
+                }
+                puts(input_file);
+                file_input = fopen(input_file, "r");
+                if(file_input == NULL)
+                {
+                    printf("\nError: Could not open input file");
+                    return -1;
+                }
+                import_file(file_input, list, &quantity);
+               
+                fclose(file_input);
+                break;
+
+            }
+            case '2':
+            {
+                printf("\nNhap ten file output can ghi ra: ");
+                while(getchar() != '\n');
+                fgets(output_file, sizeof(output_file), stdin);
+                if(output_file[strlen(output_file) - 1] == '\n')
+                {
+                     output_file[strlen(output_file) - 1] = '\0';
+                }
+                file_output = fopen(output_file, "w");
+                if(file_output == NULL)
+                {
+                    printf("\nError: Could not open output file");
+                    return -1;
+                }
+                export_file(file_output, list, quantity);
+                fclose(file_output);
+                break;
+
+            }
+            case '3':
+            {
+                printf("\n\t\tBANG DIEM SINH VIEN MON TIN DAI CUONG\n\n");
+                display(list, quantity);
+                break;
+            }
+            case '4':
+            {
+                printf("\nSee you again");
+                return 0;
+            }
+            default:
+            {
+                printf("\nBan dan nhap sai");
+            }
+        }
+        getch();
     }
-    file = fopen(input_file, "r");
-    if(file == NULL)
-    {
-        printf("\nError opening");
-        return -1;
-    }
-    unsigned int i = 0;
-    while(fscanf(file, "%s,%s", list[i].id, list[i].name ) > 0)
-    {
-        
-        printf("\nNhap diem cho sinh vien thu %u: ", i + 1);
-        scanf("%f", &list[i].point);
-        i++;
-    }
-    for(unsigned j = 0; j < i; j++)
-    {
-        printf("\nten: %s  id: %s   point: %.2f", list[j].id, list[j].name, list[j].point);
-    }
+    
+   
    
 }
-
+void menu()
+{
+    printf("\n\t\t\tPOINT MANAGEMENT SYSTEM\n\n");
+    printf("----------------------------------------------------------------\n\n");
+    printf("1.Import file list of students\n");
+    printf("2.Print list into output file\n");
+    printf("3.Display list of students fail\n");
+    printf("4.Exit program\n");
+}
 bool check_empty_file(FILE *file)
 {
     fseek(file, 0, SEEK_END);
@@ -71,22 +132,26 @@ void import_file(FILE *file, student *list, unsigned *quantity)
         printf("file is empty\n");
         return;
     }
+    
     char buffer[1024];
     char *token = NULL;
     unsigned i = 0;
     while(fgets(buffer, sizeof(buffer), file) != NULL && *quantity < MAX)
     {
-        printf("Nhap thong tin cho sinh vien thu %u: ", i + 1);
+        buffer[strlen(buffer) - 1] = '\0'; //remember this
+        printf("\nNhap thong tin cho sinh vien thu %u: ", i + 1);
         token = strtok(buffer, ",");
         if(token != NULL)
         {
             strcpy(list[i].id, token);
+           
             
         }
         token = strtok(NULL, ",");
         if(token != NULL)
         {
             strcpy(list[i].name, token);
+           
         }
         printf("\nNhap diem cua sinh vien: ");
         scanf("%f", &list[i].point);
@@ -94,6 +159,7 @@ void import_file(FILE *file, student *list, unsigned *quantity)
     }
     *quantity = i;
     token = NULL;
+    printf("\nImport file successfully");
 }
 
 void export_file(FILE *file, student *list, unsigned quantity)
@@ -117,6 +183,11 @@ void export_file(FILE *file, student *list, unsigned quantity)
 }
 void display(student *list, unsigned quantity)
 {
+    if(quantity == 0)
+    {
+        printf("\nKhong co sinh vien nao");
+        return;
+    }
     printf("%-5s %-20s %-10s %-8s\n", "STT", "Ho Ten", "Ma so", "Diem");
     printf("------------------------------------------------------------------------------------------------\n");
     for(unsigned i = 0; i < quantity; i++)
